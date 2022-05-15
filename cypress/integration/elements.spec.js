46a86af9be7db9dcb09967399749d631069c4e1b
Aleksandrs Barbins <bbarbins@gmail.com>
2022-05-15 18:20:31 +0300

    Added comments in elements.spec.js

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -4,6 +4,7 @@ context("Elements Page", () => {
     context("Text box scenarios", () => {
         it.only("formCompletion", () => {
             FormsPage.visit();
+            //Take the data from the fixture
             cy.fixture("form_text").then((data) => {
                 FormsPage.nameInput.type(data.name);
                 FormsPage.lastnameInput.type(data.last_name);
@@ -14,14 +15,17 @@ context("Elements Page", () => {
                 FormsPage.callendarYear.select(data.birth_year);
                 FormsPage.callendarMonth.select(data.birth_month);
                 FormsPage.callendarDay.click({ force: true });
+                //Press Enter to autocomplete
                 FormsPage.subjectsInput.type(data.subjects + "{enter}");
                 FormsPage.hobbiesSelect.contains(data.hobbie).click();
                 FormsPage.pictureUpload.selectFile(data.path_to_file);
                 FormsPage.addressInput.type(data.address);
+                //Press Enter to autocomplete
                 FormsPage.stateSelect.type(data.state + "{enter}");
                 FormsPage.citySelect.type(data.city + "{enter}");
                 FormsPage.submitButton.click({ force: true });
 
+                //Check if all the results can be found in the table
                 FormsPage.resultTable
                     .should("contain", data.name + " " + data.last_name)
                     .should("contain", data.email)
